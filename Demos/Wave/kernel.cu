272c941c4450758071acd31f8a06d1411da2c5d9
Iann Dang <iann1978@163.com>
2021-09-29 13:58:01 +0800

    + (demos) Finished demo wave.

--- Demos/Wave/kernel.cu
+++ Demos/Wave/kernel.cu
@@ -4,54 +4,20 @@
 
 #include <stdio.h>
 
-struct cuComplex {
-    float r;
-    float i;
-    
-    __device__ cuComplex(float a, float b) : r(a), i(b) {}
-    __device__ float magnitude2(void) 
-    {
-        return r * r + i * i;
-    }
-
-    __device__ cuComplex operator * (const cuComplex& a) 
-    {
-        return cuComplex(r * a.r - i * a.i, i * a.r + r * a.i);
-    }
-
-    __device__ cuComplex operator + (const cuComplex& a)
-    {
-        return cuComplex(r + a.r, i + a.i);
-    }
-};
 
 #define DIM 512
-__device__ int julia(int x, int y)
+
+__global__ void kernel(float* ptr, int ticks)
 {
-    const float scale = 1.5;
-    float jx = scale * (float)(DIM / 2 - x) / (DIM / 2);
-    float jy = scale * (float)(DIM / 2 - y) / (DIM / 2);
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
+    int offset = x + y * blockDim.x * gridDim.x;
 
-    cuComplex c(-0.8, 0.156);
-    cuComplex a(jx, jy);
-
-    int i = 0;
-    for (i = 0; i < 200; i++)
-    {
-        a = a * a + c;
-        if (a.magnitude2() > 1000)
-            return 0;
-    }
-    return 1;
-}
-
-__global__ void kernel(float* ptr)
-{
-    int x = blockIdx.x;
-    int y = blockIdx.y;
-    int offset = x + y * gridDim.x;
-    int juliaValue = julia(x, y);
-    ptr[offset] = juliaValue;
+    float fx = x - DIM / 2;
+    float fy = y - DIM / 2;
+    float d = sqrtf(fx * fx + fy * fy);
+    float grey = 0.5 + 0.5 * cos(d / 10.0f - ticks / 7.0f) / (d / 10.0f + 1.0f);
+    ptr[offset] = grey;
 }
 
 
@@ -63,12 +29,13 @@ bool launch_kernel(float* pos, int width, int height)
     int gridWidth = width / blockWidth;
     int gridHeight = height / blockHeight;
 
+    static int ticks = 0;
 
     cudaError_t cudaStatus = cudaSuccess;
     // execute the kernel
-    dim3 grid(width, height, 1);
-    dim3 block(1, 1, 1);
-    kernel << < grid, block >> > (pos);
+    dim3 grid(DIM/16, DIM/16, 1);
+    dim3 block(16, 16, 1);
+    kernel << < grid, block >> > (pos, ticks++);
 
 
     cudaStatus = cudaGetLastError();
