3e321f40451b125aba396c57fdf66cadffc4705b
iann <iann1978@163.com>
2023-11-06 08:56:57 +0800

    + Add Cuda122Demos.

---
+++ Cuda122Demos/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,71 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#pragma pack(push, 1)
+class A {
+    
+public:
+    int a;
+    virtual void func() { 
+        std::cout << "calling func in a."; 
+    };
+};
+
+class B {
+    
+public:
+    int b;
+    virtual void func() { 
+        std::cout << "calling func in b.";
+    };
+
+};
+
+class C : public A, public B {
+public:
+    int c;
+    virtual void func() {
+        std::cout << "calling func in c.";
+	};
+
+};
+#pragma pack(pop, 1)
+int main()
+{
+    C c;
+    A* pa = &c;
+    B* pb = &c;
+
+
+    std::cout << "Address of pa is " << pa << std::endl;
+    std::cout << "Address of pb is " << pb << std::endl;
+    std::cout << "Address of c is " << &c << std::endl;
+
+    std::cout << "Address of variable a is " << &(c.a) << std::endl;
+    std::cout << "Address of variable b is " << &(c.b) << std::endl;
+    std::cout << "Address of variable c is " << &(c.c) << std::endl;
+
+    std::cout << "Sizeof A is " << sizeof(A) << std::endl;
+std::cout << "Sizeof B is " << sizeof(B) << std::endl;
+std::cout << "Sizeof C is " << sizeof(C) << std::endl;
+
+std::cout << "Sizeof instance of A is " << sizeof(*pa) << std::endl;
+std::cout << "Sizeof instance of B is " << sizeof(*pb) << std::endl;
+std::cout << "Sizeof instance of C is " << sizeof(c) << std::endl;
+
+    pa->func();
+    pb->func();
+    //std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ Cuda122Demos/mutil-thread-opengl/mutil-thread-opengl/mutil-thread-opengl.cpp
@@ -0,0 +1,171 @@
+﻿
+// https://www.glfw.org/docs/latest/context_guide.html#context_offscreen
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <thread>
+#include <cmath>
+
+GLFWwindow* window = nullptr;
+GLFWwindow* second_window = nullptr;
+GLuint vao;
+GLuint vbo;
+bool running = true;
+
+void thread_func() {
+	glfwMakeContextCurrent(second_window);
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Failed to initialize GLEW in second thread" << std::endl;
+		return;
+	}
+
+	GLfloat vertices[] = {
+	-0.5f,-0.5f,0.0f, // top
+	0.5f,-0.5f,0.0f, // right
+	1.0f,0.5f,0.0f // left
+	};
+
+	// while (true) {
+	// 	static int angle = 0;
+	// 	angle++;
+	// 	vertices[6] = cos(angle * 3.1415926 / 180);
+	// 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	// 	// convert to float array
+	// 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// }
+
+
+	while (running) {
+		static int angle = 0;
+		angle++;
+		vertices[6] = cos(angle * 3.1415926 / 180);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		// convert to float array
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+		// glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		// glClear(GL_COLOR_BUFFER_BIT);
+		// glColor3f(0, 1, 0);
+		// glEnableVertexAttribArray(0);
+
+		// glVertexAttribPointer(
+		// 	0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+		// 	3,                  // size
+		// 	GL_FLOAT,           // type
+		// 	GL_FALSE,           // normalized?
+		// 	0,                  // stride
+		// 	(void*)0            // array buffer offset
+		// );
+		// glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		// draw triangle
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// glBegin(GL_TRIANGLES);
+		// glVertex3f(-0.5f,-0.5f,0.0f);
+		// glVertex3f(0.5f,-0.5f,0.0f);
+		// glVertex3f(0.0f,0.5f,0.0f);
+		// glEnd();
+		glfwSwapBuffers(second_window);
+		// glfwPollEvents();
+	}
+
+}
+int main() {
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return 1;
+	}
+
+
+
+
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(640, 480, "GLFW window", nullptr, nullptr);
+	glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
+	second_window = glfwCreateWindow(640, 480, "Second Window", NULL, window);
+	if (!window) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Failed to initialize GLEW" << std::endl;
+		return 1;
+	}
+
+	// get version of opengl
+	std::cout << glGetString(GL_VERSION) << std::endl;
+	// std::cout << glGetInteger(GLEW_VERSION_MAJOR) << std::endl;
+
+	// create vao
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+
+	// create vbo
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	// create a triangle
+	GLfloat vertices[] = {
+		-0.5f,-0.5f,0.0f, // top
+		0.5f,-0.5f,0.0f, // right
+		0.0f,0.5f,0.0f // left
+	};
+
+	// convert to float array
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	std::thread th(thread_func);
+
+
+
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glColor3f(0, 1, 0);
+		glEnableVertexAttribArray(0);
+
+		glVertexAttribPointer(
+			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		// draw triangle
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// glBegin(GL_TRIANGLES);
+		// glVertex3f(-0.5f,-0.5f,0.0f);
+		// glVertex3f(0.5f,-0.5f,0.0f);
+		// glVertex3f(0.0f,0.5f,0.0f);
+		// glEnd();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	};
+	running = false;
+	th.join();
+	glfwTerminate();
+
+	int a = 0;
+	int b = 1;
+
+	std::cout << "hello world!!" << std::endl;
+	return 0;
+}
