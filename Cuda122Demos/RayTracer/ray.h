fd7cbfa479ee21dfee4fe61c1948c334f3136431
iann <iann1978@163.com>
2023-11-07 08:40:05 +0800

    + (RayTracer) Add Simple Sphere.

--- Cuda122Demos/RayTracer/ray.h
+++ Cuda122Demos/RayTracer/ray.h
@@ -11,7 +11,7 @@ public:
 
     __device__ ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
 
-    //point3 origin() const { return orig; }
+    __device__ point3 origin() const { return orig; }
     __device__ vec3 direction() const { return dir; }
 
     //point3 at(double t) const {
